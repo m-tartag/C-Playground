79595e679fd57096f1fe14eb9c9d7b0f19e7fd89
m-tartag <mattbrs51@gmail.com>
2019-12-10 12:17:09 -0500

    binary trees

---
+++ Practice/binaryTree.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+//create a binary node
+//should have int value
+//should have 2 branches
+
+typedef struct BNODE
+{
+    int value;
+    struct BNODE
+}
+
+int
+main(void)
+{
+    printf("hello\n")
+}
